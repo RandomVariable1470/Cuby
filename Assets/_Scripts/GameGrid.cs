1afcc43ab0111f1bc967cce3bddec895a3ea0229
Naman Sharma <naman5421sharma@gmail.com>
2023-12-14 21:55:53 +0530

    Changes 1.05
    Code Optimization

--- Assets/_Scripts/GameGrid.cs
+++ Assets/_Scripts/GameGrid.cs
@@ -1,6 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using System.Collections;
 
 public class GameGrid : MonoBehaviour
 {
@@ -8,48 +8,55 @@ public class GameGrid : MonoBehaviour
 
     [SerializeField] private int height = 6;
     [SerializeField] private int width = 3;
-    [SerializeField] private int gridSpaceSize = 5;
-    [SerializeField] private float delayTime = 0.01f; 
+    [SerializeField] private float gridSpaceSize = 5f;
+    [SerializeField] private float delayTime = 0.01f;
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip audioClip;
 
     private GameObject[,] gameGrid;
 
-    private void Start() 
+    private void Start()
     {
-        StartCoroutine(CreateGrid());
+        try
+        {
+            StartCoroutine(CreateGrid());
+        }
+        catch (Exception e)
+        {
+            Debug.LogError($"Error: {e.Message}");
+        }
     }
 
     private IEnumerator CreateGrid()
     {
-        gameGrid = new GameObject[height, width];
-
         if (gridCellPrefab == null)
         {
-            Debug.LogError("ERROR: Grid Cell Prefab on the Game Grid has not been assigned");
-            yield return null;
+            throw new Exception("Grid Cell Prefab on the Game Grid has not been assigned");
         }
+
+        gameGrid = new GameObject[height, width];
 
         for (int y = 0; y < height; y++)
         {
             for (int x = 0; x < width; x++)
             {
-                gameGrid[y, x] = Instantiate(gridCellPrefab, new Vector3(x * gridSpaceSize, 0, y * gridSpaceSize), Quaternion.identity);
-                gameGrid[y, x].GetComponent<GridCell>().SetPosition(x, y);
-                gameGrid[y, x].transform.parent = transform; 
-                gameGrid[y, x].gameObject.name = "Grid Space ( X: " + x.ToString() + ", Y: " + y.ToString() + ") ";
+                GameObject newCell = Instantiate(gridCellPrefab, new Vector3(x * gridSpaceSize, 0, y * gridSpaceSize), Quaternion.identity);
+                newCell.GetComponent<GridCell>().SetPosition(x, y);
+                newCell.transform.parent = transform;
+                newCell.name = $"Grid Space (X: {x}, Y: {y})";
 
-                // Ensure audio source and clip are not null before playing audio
                 if (audioSource != null && audioClip != null)
                 {
                     audioSource.PlayOneShot(audioClip);
-                    yield return new WaitForSeconds(delayTime);
                 }
+
+                gameGrid[y, x] = newCell;
+
+                yield return new WaitForSeconds(delayTime);
             }
         }
     }
 
-    // Gets Grid Position From World Position
     public Vector2Int GetGridPosFromWorld(Vector3 worldPosition)
     {
         int x = Mathf.FloorToInt(worldPosition.x / gridSpaceSize);
@@ -61,11 +68,10 @@ public class GameGrid : MonoBehaviour
         return new Vector2Int(x, y);
     }
 
-    // Gets The World Position Of A Grid Position
     public Vector3 GetWorldPosFromGrid(Vector2Int gridPos)
     {
-        int x = gridPos.x * gridSpaceSize;
-        int y = gridPos.y * gridSpaceSize;
+        int x = gridPos.x * (int)gridSpaceSize;
+        int y = gridPos.y * (int)gridSpaceSize;
 
         return new Vector3(x, 0, y);
     }
